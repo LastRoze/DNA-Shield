26fa09b4632385797288d7c83dd9472d02fbc76f
Yoga <LastRoze@users.noreply.github.com>
2020-04-17 08:28:53 +0700

    Create DNA-Shield.meta.js

---
+++ DNA-Shield.meta.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         DNA Shield
+// @namespace    DNA Shield
+// @description  Dominion With Domination
+// @copyright    ©2020 - Yoga Budiman
+// @version      1.0
+// @author       Last Roze
+// @homepageURL  https://github.com/LastRoze/
+// @supportURL   https://linkedin.com/in/lastroze
+// @updateURL    https://github.com/LastRoze/DNA-Shield/raw/master/ZUZU-Menu.meta.js
+// @downloadURL  https://github.com/LastRoze/DNA-Shield/raw/master/ZUZU-Menu.user.js
+// @icon         https://github.com/LastRoze/DNA-Shield/blob/master/DNA.png
+// @require      https://code.jquery.com/jquery-latest.js
+// @run-at       document-start
+// @match        *://*/*
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.deleteValue
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @noframes
+// ==/UserScript==
