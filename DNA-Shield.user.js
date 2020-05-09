f91fe5cf21d962ee632d9b8ee3328aa8c43e05a8
Yoga <LastRoze@users.noreply.github.com>
2020-05-09 11:40:53 +0700

    Update DNA-Shield.user.js

--- DNA-Shield.user.js
+++ DNA-Shield.user.js
@@ -3,7 +3,7 @@
 // @namespace    DNA Shield
 // @description  Dominion With Domination
 // @copyright    ©2020 - Yoga Budiman
-// @version      2.1
+// @version      2.2
 // @author       Last Roze
 // @homepageURL  https://github.com/LastRoze/
 // @supportURL   https://linkedin.com/in/lastroze
@@ -48,17 +48,3 @@ document.addEventListener("turbolinks:load", function() {
     'use strict';
     window.seconds = -1;
 })();
-
-var styles = [
-    'background: linear-gradient(#D33106, #571402)'
-    , 'border: 1px solid #3E0E02'
-    , 'color: white'
-    , 'display: block'
-    , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
-    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
-    , 'line-height: 40px'
-    , 'text-align: center'
-    , 'font-weight: bold'
-].join(';');
-
-console.log('%c DNA Shield Loaded !', styles);
