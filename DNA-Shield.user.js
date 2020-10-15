baa00ef92daa5efa959a5e3a37ee61a61d7c721e
Yoga Budiman <LastRoze@users.noreply.github.com>
2020-10-16 03:00:40 +0700

    Update DNA-Shield.user.js

--- DNA-Shield.user.js
+++ DNA-Shield.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	DNA Shield
 // @namespace	DNA Shield
-// @version	6.3
+// @version	6.4
 // @author	Last Roze
 // @description	Dominion With Domination
 // @copyright	©2020 - Yoga Budiman
@@ -44,48 +44,6 @@
 (function() {
     window.seconds = -1;
 })();
-
-document.addEventListener("readystatechange",function(){
-    var DNA1=document.createElement("script");
-    DNA1.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/jquery.lazy.min.js")
-    DNA1.setAttribute("id","DNA1")
-    document.getElementsByTagName("head").item(0).appendChild(DNA1)
-},!1);
-
-document.addEventListener("readystatechange",function(){
-    var DNA2=document.createElement("script");
-    DNA2.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/jquery.lazy.plugins.min.js")
-    DNA2.setAttribute("id","DNA2")
-    document.getElementsByTagName("head").item(0).appendChild(DNA2)
-},!1);
-
-document.addEventListener("readystatechange",function(){
-    var DNA3=document.createElement("script");
-    DNA3.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/plugins/jquery.lazy.ajax.min.js")
-    DNA3.setAttribute("id","DNA3")
-    document.getElementsByTagName("head").item(0).appendChild(DNA3)
-},!1);
-
-document.addEventListener("readystatechange",function(){
-    var DNA4=document.createElement("script");
-    DNA4.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/plugins/jquery.lazy.av.min.js")
-    DNA4.setAttribute("id","DNA4")
-    document.getElementsByTagName("head").item(0).appendChild(DNA4)
-},!1);
-
-document.addEventListener("readystatechange",function(){
-    var DNA5=document.createElement("script");
-    DNA5.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/plugins/jquery.lazy.iframe.min.js")
-    DNA5.setAttribute("id","DNA5")
-    document.getElementsByTagName("head").item(0).appendChild(DNA5)
-},!1);
-
-document.addEventListener("readystatechange",function(){
-    var DNA6=document.createElement("script");
-    DNA6.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/plugins/jquery.lazy.noop.min.js")
-    DNA6.setAttribute("id","DNA6")
-    document.getElementsByTagName("head").item(0).appendChild(DNA6)
-},!1);
 
 (function() {
     const node = document.getElementsByTagName('html');
