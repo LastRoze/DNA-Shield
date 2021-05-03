bf0949ab5a92e4be9fe99301d450a4c2a6424783
Yoga Budiman <LastRoze@users.noreply.github.com>
2021-05-03 19:38:50 +0700

    Update DNA-Shield.meta.js

--- DNA-Shield.meta.js
+++ DNA-Shield.meta.js
@@ -1,42 +1,42 @@
 // ==UserScript==
-// @name	DNA Shield
+// @name		DNA Shield
 // @namespace	DNA Shield
-// @version	6.6
-// @author	Last Roze
+// @version		7.0
+// @author		Last Roze
 // @description	Dominion With Domination
 // @copyright	©2021 - Yoga Budiman
 // @homepage	https://github.com/LastRoze/
 // @homepageURL	https://github.com/LastRoze/
-// @website	https://lastroze.github.io/
-// @source	https://github.com/LastRoze/DNA-Shield
-// @icon	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @iconURL	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @website		https://lastroze.github.io/
+// @source		https://github.com/LastRoze/DNA-Shield
+// @icon		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @iconURL		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
 // @defaulticon	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @icon64	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @icon64		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
 // @icon64URL	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
 // @updateURL	https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.meta.js
 // @downloadURL	https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.user.js
 // @supportURL	https://lastroze.github.io/
-// @include	*
-// @exclude	https://docs.google.com/*
-// @connect	*
-// @run-at	document-start
-// @grant	GM_addStyle
-// @grant	GM_deleteValue
-// @grant	GM_download
-// @grant	GM_getResourceText
-// @grant	GM_getResourceURL
-// @grant	GM_getValue
-// @grant	GM_listValues
-// @grant	GM_notification
-// @grant	GM_openInTab
-// @grant	GM_registerMenuCommand
-// @grant	GM_setClipboard
-// @grant	GM_setValue
-// @grant	GM_unregisterMenuCommand
-// @grant	GM_xmlhttpRequest
-// @grant	unsafeWindow
-// @grant	window.close
-// @grant	window.focus
-// @grant	window.onurlchange
+// @include		*
+// @exclude		https://*.google.com/*
+// @connect		*
+// @run-at		document-start
+// @grant		GM_addStyle
+// @grant		GM_deleteValue
+// @grant		GM_download
+// @grant		GM_getResourceText
+// @grant		GM_getResourceURL
+// @grant		GM_getValue
+// @grant		GM_listValues
+// @grant		GM_notification
+// @grant		GM_openInTab
+// @grant		GM_registerMenuCommand
+// @grant		GM_setClipboard
+// @grant		GM_setValue
+// @grant		GM_unregisterMenuCommand
+// @grant		GM_xmlhttpRequest
+// @grant		unsafeWindow
+// @grant		window.close
+// @grant		window.focus
+// @grant		window.onurlchange
 // ==/UserScript==
