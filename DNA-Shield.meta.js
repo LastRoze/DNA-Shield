e10fbbf7cd56fad97ab68de17871009685b67113
Yoga Budiman <LastRoze@users.noreply.github.com>
2021-05-31 03:19:12 +0700

    Update DNA-Shield.meta.js

--- DNA-Shield.meta.js
+++ DNA-Shield.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		DNA Shield
 // @namespace	DNA Shield
-// @version		7.0
+// @version		7.1
 // @author		Last Roze
 // @description	Dominion With Domination
 // @copyright	©2021 - Yoga Budiman
@@ -18,7 +18,7 @@
 // @downloadURL	https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.user.js
 // @supportURL	https://lastroze.github.io/
 // @include		*
-// @exclude		https://*.google.com/*
+// @exclude		
 // @connect		*
 // @run-at		document-start
 // @grant		GM_addStyle
