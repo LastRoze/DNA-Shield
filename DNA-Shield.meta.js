78ee21e67c71952d0a026325cc51ed543c6a47d4
Yoga Budiman <LastRoze@users.noreply.github.com>
2025-09-18 21:18:49 +0700

    Improve resource prioritization and keepalive

--- DNA-Shield.meta.js
+++ DNA-Shield.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                DNA Shield
 // @namespace           DNA Shield
-// @version             1.2
+// @version             1.3
 // @author              Last Roze
 // @description         Dominion With Domination
 // @copyright           ©2021 - 2025 // Yoga Budiman
