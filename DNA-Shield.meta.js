ebc1656a4560d986f247b5cfa8ca4b3c10512b4f
Yoga Budiman <LastRoze@users.noreply.github.com>
2026-09-14 11:59:55 +0700

    Update metadata for DNA Shield to version 2.0
    Updated metadata for version 2.0, including author, description, and permissions.

--- DNA-Shield.meta.js
+++ DNA-Shield.meta.js
@@ -1,41 +1,23 @@
 // ==UserScript==
-// @name		DNA Shield
-// @namespace	DNA Shield
-// @version		1.0
-// @author		Last Roze
-// @description	Dominion With Domination
-// @copyright	©2021 - Yoga Budiman
-// @homepage	https://github.com/LastRoze/
-// @homepageURL	https://github.com/LastRoze/
-// @website		https://lastroze.github.io/
-// @source		https://github.com/LastRoze/DNA-Shield
-// @icon		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @iconURL		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @defaulticon	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @icon64		https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @icon64URL	https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
-// @updateURL	https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.meta.js
-// @downloadURL	https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.user.js
-// @supportURL	https://lastroze.github.io/
-// @include		*
-// @connect		*
-// @run-at		document-start
-// @grant		GM_addStyle
-// @grant		GM_deleteValue
-// @grant		GM_download
-// @grant		GM_getResourceText
-// @grant		GM_getResourceURL
-// @grant		GM_getValue
-// @grant		GM_listValues
-// @grant		GM_notification
-// @grant		GM_openInTab
-// @grant		GM_registerMenuCommand
-// @grant		GM_setClipboard
-// @grant		GM_setValue
-// @grant		GM_unregisterMenuCommand
-// @grant		GM_xmlhttpRequest
-// @grant		unsafeWindow
-// @grant		window.close
-// @grant		window.focus
-// @grant		window.onurlchange
+// @name         DNA Shield
+// @namespace    DNA Shield
+// @version      2.0
+// @author       Last Roze
+// @description  Dominion With Domination — Universal Zero-Grant Instant UI Engine
+// @copyright    ©2020-2026 Yoga Budiman
+// @homepage     https://github.com/LastRoze/
+// @homepageURL  https://github.com/LastRoze/
+// @website      https://lastroze.github.io/
+// @source       https://github.com/LastRoze/DNA-Shield
+// @icon         https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @iconURL      https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @defaulticon  https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @icon64       https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @icon64URL    https://github.com/LastRoze/DNA-Shield/blob/master/DNA.jpg?raw=true
+// @updateURL    https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.meta.js
+// @downloadURL  https://github.com/LastRoze/DNA-Shield/raw/master/DNA-Shield.user.js
+// @supportURL   https://lastroze.github.io/
+// @match        *://*/*
+// @run-at       document-start
+// @grant        none
 // ==/UserScript==
